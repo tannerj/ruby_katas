aa0b2f2afd04ee93c98f41a45a05e7a194774e6b
Joel Tanner Jones <tannerjones10@gmail.com>
2015-02-04 16:08:07 -0500

    failing haystack[0] tests.

--- karate_chop/karate_chop.rb
+++ karate_chop/karate_chop.rb
@@ -7,11 +7,13 @@ module KarateChop
 
   private
   def self.do_chop(target, haystack, min, max)
-    return -1 if min > max
+    return -1 if min > max #target miss, return
     mid = min + ((max - min) / 2)
-    return mid if target == haystack[mid]
-    min = mid + 1 if target > haystack[mid]
-    max = mid - 1 if target < haystack[mid]
+    local_mid = (haystack.length - 1 ) / 2
+    return mid if target == haystack[local_mid] #found target, return key
+    min = mid + 1 if target > haystack[local_mid]
+    max = mid - 1 if target < haystack[local_mid]
+    haystack.slice!(min..max)
     do_chop(target, haystack, min, max)
   end
 end
