0a3a1cc160ab536deb977af82360b664a3128b79
Joel Tanner Jones <tannerjones10@gmail.com>
2015-02-11 15:42:02 -0500

    Failing 1-100 array test.

--- karate_chop/karate_chop.rb
+++ karate_chop/karate_chop.rb
@@ -1,17 +1,25 @@
-module KarateChop  
-
+module KarateChop
   module_function
+  
   def chop(target, haystack)
-    return do_chop(target, haystack, 0, haystack.length - 1)
-  end
-
-  private
-  def self.do_chop(target, haystack, min, max)
-    return -1 if min > max
-    mid = min + ((max - min) / 2)
-    return mid if target == haystack[mid]
-    min = mid + 1 if target > haystack[mid]
-    max = mid - 1 if target < haystack[mid]
-    do_chop(target, haystack, min, max)
+    return -1 if haystack.length == 0
+    #catch target out of bounds 
+    return -1 if target < haystack.min || target > haystack.max
+    mid = (haystack.length / 2.0).ceil
+    return mid - 1 if target == haystack[mid - 1]
+    haystack_front_length = mid
+    while haystack.length > 1
+      haystack_front = haystack.slice!(0, haystack_front_length)
+      return mid - 1 if haystack_front.max == target
+      if haystack_front.max > target
+        haystack_front_length = (haystack_front.length / 2.0).ceil
+        mid -= haystack_front_length
+        haystack = haystack_front 
+      else 
+        haystack_front_length = (haystack.length / 2.0).ceil
+        mid += haystack_front_length
+      end
+    end
+    return haystack.max == target ? mid - 1 : -1
   end
 end
---
+++ karate_chop/karate_chop_runner.rb
@@ -0,0 +1,5 @@
+#! /usr/bin/env ruby
+
+require_relative 'karate_chop'
+
+puts KarateChop::chop(1, [1,2,3,4])
